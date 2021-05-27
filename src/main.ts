e45b3da738c156c93aecd28c3fbe059af38c6310
SettingDust <settingdust@gmail.com>
2021-05-27 17:52:35 +0800

    初始提交

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import TelegramBot, {InputMediaPhoto, ParseMode} from 'node-telegram-bot-api'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const chatId: string = core.getInput('chatId')
+    const botToken: string = core.getInput('botToken')
+    const content: string = core.getInput('content')
+    const photoUrls: string = core.getInput('photoUrls')
+    const format: ParseMode =
+      (core.getInput('format') as ParseMode) || 'Markdown'
+    const disableWebPagePreview: boolean =
+      core.getInput('disableWebPagePreview') === 'true' || false
+    const disableNotification: boolean =
+      core.getInput('disableNotification') === 'true' || false
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const bot = new TelegramBot(botToken)
+    if (photoUrls) {
+      bot.sendMediaGroup(
+        chatId,
+        photoUrls.split('\n').map(
+          (it, index) =>
+            ({
+              type: 'photo',
+              media: it,
+              caption: index === 0 ? content : undefined,
+              parse_mode: format
+            } as InputMediaPhoto)
+        ),
+        {
+          disable_notification: disableNotification
+        }
+      )
+    } else if (content)
+      bot.sendMessage(chatId, content, {
+        parse_mode: format,
+        disable_notification: disableNotification,
+        disable_web_page_preview: disableWebPagePreview
+      })
   } catch (error) {
     core.setFailed(error.message)
   }
